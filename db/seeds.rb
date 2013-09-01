54ad9d7668c89abc3b37c03bdb8a32cc3ecf91ad
Julien Bergner <julien@bergner.fr>
2013-09-01 23:55:11 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-  puts 'role: ' << role
-end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
-user.add_role :admin
+BallTable.create!( :caption => "Tisch 1", :floor => "1", :type => "rect", :price => "0", :position_x => "310", :position_y => "135", :radius => "0", :width => "75", :height => "50")
+BallTable.create!( :caption => "Tisch 2", :floor => "1", :type => "rect", :price => "0", :position_x => "310", :position_y => "15", :radius => "0", :width => "75", :height => "100")
+BallTable.create!( :caption => "Tisch 3", :floor => "1", :type => "circle", :price => "0", :position_x => "235", :position_y => "55", :radius => "40", :width => "0", :height => "0")
+BallTable.create!( :caption => "Tisch 4", :floor => "1", :type => "rect", :price => "0", :position_x => "100", :position_y => "40", :radius => "0", :width => "50", :height => "50")
+BallTable.create!( :caption => "Tisch 5", :floor => "1", :type => "rect", :price => "0", :position_x => "15", :position_y => "130", :radius => "0", :width => "100", :height => "75")
+BallTable.create!( :caption => "Tisch 6", :floor => "1", :type => "circle", :price => "0", :position_x => "65", :position_y => "265", :radius => "40", :width => "0", :height => "0")
+BallTable.create!( :caption => "Tisch 7", :floor => "1", :type => "rect", :price => "0", :position_x => "15", :position_y => "325", :radius => "0", :width => "100", :height => "75")
+BallTable.create!( :caption => "Tisch 8", :floor => "1", :type => "circle", :price => "0", :position_x => "75", :position_y => "460", :radius => "40", :width => "0", :height => "0")
+BallTable.create!( :caption => "Tisch 9", :floor => "1", :type => "rect", :price => "0", :position_x => "150", :position_y => "435", :radius => "0", :width => "75", :height => "100")
+BallTable.create!( :caption => "Tisch 10", :floor => "1", :type => "circle", :price => "0", :position_x => "290", :position_y => "485", :radius => "40", :width => "0", :height => "0")
+
+BallTable.create!( :caption => "Tisch 11", :floor => "2", :type => "circle", :price => "0", :position_x => "315", :position_y => "55", :radius => "40", :width => "0", :height => "0")
+BallTable.create!( :caption => "Tisch 12", :floor => "2", :type => "rect", :price => "0", :position_x => "170", :position_y => "15", :radius => "0", :width => "75", :height => "100")
+BallTable.create!( :caption => "Tisch 13", :floor => "2", :type => "circle", :price => "0", :position_x => "100", :position_y => "70", :radius => "40", :width => "0", :height => "0")
+BallTable.create!( :caption => "Tisch 14", :floor => "2", :type => "rect", :price => "0", :position_x => "15", :position_y => "130", :radius => "0", :width => "100", :height => "75")
+BallTable.create!( :caption => "Tisch 15", :floor => "2", :type => "circle", :price => "0", :position_x => "65", :position_y => "265", :radius => "40", :width => "0", :height => "0")
+BallTable.create!( :caption => "Tisch 16", :floor => "2", :type => "rect", :price => "0", :position_x => "15", :position_y => "325", :radius => "0", :width => "100", :height => "75")
+BallTable.create!( :caption => "Tisch 17", :floor => "2", :type => "circle", :price => "0", :position_x => "75", :position_y => "460", :radius => "40", :width => "0", :height => "0")
+BallTable.create!( :caption => "Tisch 18", :floor => "2", :type => "rect", :price => "0", :position_x => "145", :position_y => "485", :radius => "0", :width => "75", :height => "50")
+BallTable.create!( :caption => "Tisch 19", :floor => "2", :type => "rect", :price => "0", :position_x => "335", :position_y => "270", :radius => "0", :width => "50", :height => "75")
+BallTable.create!( :caption => "Tisch 20", :floor => "2", :type => "rect", :price => "0", :position_x => "215", :position_y => "270", :radius => "0", :width => "100", :height => "75")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
