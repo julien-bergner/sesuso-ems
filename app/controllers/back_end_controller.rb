05396ab9cff7884611b0db0c4baad8391528e7de
Julien Bergner <julien@bergner.fr>
2013-09-18 01:32:28 +0200

    Add abilities for overview

--- app/controllers/back_end_controller.rb
+++ app/controllers/back_end_controller.rb
@@ -1,8 +1,8 @@
 class BackEndController < ApplicationController
   before_filter :authenticate_user!
-  load_and_authorize_resource
 
   def overview
+    authorize! :show, :overview
     @customers = Customer.all
   end
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,13 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
       can :manage, :all
+      can :show, :overview
     end
+
+    if user.has_role? :manager
+      can :show, :overview
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
