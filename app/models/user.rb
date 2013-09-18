0cd23d339796e0dd3ae04e06c6b7fbdb9e7932b8
Julien Bergner <julien@bergner.fr>
2013-09-18 15:50:23 +0200

    Prepare auth with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,25 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username,
+            :uniqueness => {
+                :case_sensitive => false
+            }
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :login
+
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
   
 end
