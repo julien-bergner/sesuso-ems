e30d7157b1a022387d5563c0ee29bd86d49cf47b
Julien Bergner <julien@bergner.fr>
2013-09-18 16:49:15 +0200

    Define German as default language

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130908214949) do
+ActiveRecord::Schema.define(:version => 20130918135902) do
 
   create_table "ball_tables", :force => true do |t|
     t.string   "caption"
@@ -110,7 +110,7 @@ ActiveRecord::Schema.define(:version => 20130908214949) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
