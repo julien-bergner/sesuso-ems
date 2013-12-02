27a49f62a123f4a3ee83f9b7f23f8db3c1bf0e86
Julien Bergner <julien@bergner.fr>
2013-12-03 00:04:32 +0100

    Pretty up buy_gift_card_workflow views

--- app/controllers/buy_gift_card_workflow_controller.rb
+++ app/controllers/buy_gift_card_workflow_controller.rb
@@ -9,11 +9,17 @@ class BuyGiftCardWorkflowController < ApplicationController
   end
 
   def receive_gift_card_number
-    redirect_to :action => "enter_amount"
+    product = Product.find_by_number(params[:gift_card][:number])
+    if product.nil?
+      flash[:error] = 'Entschuldigung. Ihre Gutscheinnummer wurde nicht gefunden. <br>Bitte kontrollieren Sie Ihre Eingabe oder wenden Sie sich an dance-discounter@sesuso.de!'.html_safe
+      redirect_to :action => "enter_gift_card_number"
+    else
+      redirect_to :action => "enter_gift_card_amount", :product_id => product.id
+    end
 
   end
 
-  def enter_amount
+  def enter_gift_card_amount
 
   end
 
@@ -22,6 +28,10 @@ class BuyGiftCardWorkflowController < ApplicationController
   end
 
   def select_payment_method
+
+  end
+
+  def show_bank_data
 
   end
 
