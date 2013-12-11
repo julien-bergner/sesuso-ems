574eda7a36b3714c506b1d2841fd33d012cfb7bb
Julien Bergner <julien@bergner.fr>
2013-12-11 04:22:28 +0100

    Fix migration

--- db/migrate/20131211031237_add_gift_card_number_id_to_order_items.rb
+++ db/migrate/20131211031237_add_gift_card_number_id_to_order_items.rb
@@ -1,5 +1,5 @@
 class AddGiftCardNumberIdToOrders < ActiveRecord::Migration
   def change
-    add_column :orders, :gift_card_number_id, :integer
+    add_column :order_items, :gift_card_number_id, :integer
   end
 end
