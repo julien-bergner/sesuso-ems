4efee79741dda74c6c9367e90644043ba6cd1e33
Julien Bergner <julien@bergner.fr>
2014-02-11 00:40:19 +0100

    Add relationship between order item and gift card number

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,5 @@ class OrderItem < ActiveRecord::Base
   attr_accessible :order_id, :product_id, :gift_card_number_id, :quantity
   belongs_to :order
   belongs_to :product
+  has_one :gift_card_number
 end
